4e8efa928d2cb0a2acbe2a8947609aa9bffdf253
namiron <102922048+namiron@users.noreply.github.com>
2024-07-29 18:12:07 +0300

    tab_red

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+};
